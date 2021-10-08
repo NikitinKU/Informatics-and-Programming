4b0848371299e655657281b3bebd558c015cd54a
NikitinKU <clownzzzz@mail.ru>
2021-10-08 23:19:02 +0300

    добавлено неполное решение Task2 (Homework). Есть вопросы.

--- HW/HomeworkTasks/Task2hw(GuessTheNumberGame)/Task2hw(random_game)/GuessTheNumber.c
+++ HW/HomeworkTasks/Task2hw(GuessTheNumberGame)/Task2hw(random_game)/GuessTheNumber.c
@@ -1,91 +1,111 @@
-#define _CRT_RAND_S
+// Copyright 2021 Kirill Nikitin
+
+/*
+/Task2:
+/*Разработать программу «Угадай число».
+В программе должно быть предусмотрено два режима.
+Режим 1. Программа «загадывает» случайное число из диапазона от 1 до 1000.
+Пользователь должен вводить отгадки, на которые программа сообщает: «загаданное число больше», «загаданное число меньше», «угадали».
+Работа завершается, когда пользователь угадает число. Также программа должна подсчитывать число попыток и выводить его в конце работы.
+Режим 2. Пользователь загадывает число из диапазона от 1 до 1000 и вводит его.
+Программа пытается «угадать» число, выводя на экран отгадки, на которые пользователь вводит >, < или =.
+Работа завершается, когда программа угадает число. Также программа должна подсчитывать число попыток и выводить его в конце работы.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <locale.h>
-
-//Task2:
-/*Разработать программу «Угадай число».
-В программе должно быть предусмотрено два режима. 
-Режим 1. Программа «загадывает» случайное число из диапазона от 1 до 1000. 
-Пользователь должен вводить отгадки, на которые программа сообщает: «загаданное число больше», «загаданное число меньше», «угадали». 
-Работа завершается, когда пользователь угадает число. Также программа должна подсчитывать число попыток и выводить его в конце работы. 
-Режим 2. Пользователь загадывает число из диапазона от 1 до 1000 и вводит его. 
-Программа пытается «угадать» число, выводя на экран отгадки, на которые пользователь вводит >, < или =. 
-Работа завершается, когда программа угадает число. Также программа должна подсчитывать число попыток и выводить его в конце работы.*/
 
 int main() {
-	setlocale(LC_ALL, "Russian");
 
 	//input
-	int c, count_of_tries, attempt, ModeOfTheGame; 
-	/*
-	c - a random number in range of 1 to 1000 that the computer picks and gives you for guessing it 
-	count_of_tries - all attempts the player did guessing the number
-	attempt - player's attempt that he inputs every time
-	ModeOfTheGame is literally the mode of the game
-					 1-st mode: You gotta guess the number (case 1)
-					 2-nd mode: The computer gotta guess the number (case 2)
-	*/
+	int c, count_of_tries = 0, attempt = 0, ModeOfTheGame = 0; 
+	int num_for_guess = 0; //number that the computer will guess
 
-	printf("Choose the mode of the game: 1-st mode: You gotta guess the number\n\t\t\t2-nd mode: The computer gotta guess the number");
+	char equel = '=';
+	char greater = '>';
+	char lower = '<';
 
-	//actions
-		switch (ModeOfTheGame)
-		{
+	printf("Choose the mode of the game: 1-st mode: You got to guess the number\n");
+	printf("\t\t\t     2-nd mode: The computer got to guess the number\n");
+	scanf_s("%d", &ModeOfTheGame);
+
+	switch (ModeOfTheGame) {
 		case 1:
 			printf("Welcome!\nYou gotta guess the number that the computer picked in range between 1 to 1000\n");
 			srand(time(NULL));
-			count_of_tries = 0; //Обнуление счетчика попыток игрока отгадать число
 			c = 1 + rand() % 1000; //Я беру остаток от деления диапозона функции rand() на 1000 и прибавляю 1, чтобы установить нужный диапозон, т.е. от 1 до 1000.
 			//printf("%d\n", c); //Число, загаданное компьютером
 			printf("The computer has picked the number in range of 1 to 1000!\nTry to guess it! Good luck!\nEnter the number You think the computer has picked: ");
-			again:
+		again:
 			scanf_s("%d", &attempt);
-			while (attempt > 0)
-			{
-				if ((attempt >= 1) && (attempt <= 1000))
-				{
-					if (attempt == c)
-					{
+			while (attempt > 0) {
+				if ((attempt >= 1) && (attempt <= 1000)) {
+					if (attempt == c) {
 						printf("\nCongratulations! You guessed the number!\nThe number for guessing was: %d\n", c);
-						printf("By the way the quantity of tries is: %d\n", count_of_tries);
-						count_of_tries += 1;
+						count_of_tries++;
 						break;
 					}
 					else
-						if (attempt > c)
-						{
+						if (attempt > c) {
 							printf("The number < %d, try again: ", attempt);
-							count_of_tries += 1;
+							count_of_tries++;
 							goto again;
 							break;
 						}
-						else
-						{
+						else {
 							printf("The number > %d, try again: ", attempt);
-							count_of_tries += 1;
+							count_of_tries++;
 							goto again;
 							break;
 						}
 				}
-				else
-				{
+				else {
 					printf("You entered the wrong number! The range is 1 to 1000.\nEnter the number again, please: ");
 					goto again;
 				}
-		break;
 			}
+			break;
+
 		case 2:
-			printf("Welcome!\nThe computer will try to guess the number that you'd pick for guessing\nChoose the number from 1 to 1000: ");
-			scanf_s("%d", &c);
-			count_of_tries = 0; //Обнуление счетчика попыток игрока отгадать число
-			while () 
-			{
-				srand(time(NULL));
+			printf("Welcome!\nThe computer will try to guess the number that you'd pick for guessing\n");
+			printf("Choose the number from 1 to 1000: ");
+		again1:
+			scanf_s("%d", &num_for_guess);
+			srand(time(NULL));
+			c = 1 + rand() % 1000;
+
+			printf("Computer thinks this number is %d\n", c);
+			printf("Is it right?\n");
+			while (num_for_guess > 0) {
+				/*if ((num_for_guess >= 1) && (num_for_guess <= 1000)) {
+					if (c == num_for_guess) {
+						printf("The computer guessed the number! It was %d\n", num_for_guess);
+						count_of_tries++;
+					}
+					else 
+						if (c > num_for_guess) {
+							printf("");
+							count_of_tries++;
+						}
+						else {
+							printf("");
+							count_of_tries++;
+						}
+				}
+				else {
+					printf("You entered the wrong number! The range is 1 to 1000.\nEnter the number again, please: ");
+					goto again1;
+				}*/
 			}
-			
-		}
-	return 0;
+			break;
+
+		default:
+			printf("You entered the variant that doesn't exist! Try again, please.\n");
+			break;
 	}
+	printf("By the way the quantity of tries is: %d\n", count_of_tries);
+	printf("Goodbye!\n");
+	return 0;
+}
